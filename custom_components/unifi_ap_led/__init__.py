8a0e813d15c3f2999c63cdeb2b8a154e52fa4453
Adam Cowell <lurch@lurch.uk>
2025-06-22 15:30:20 +0100

    Update __init__.py

--- custom_components/unifi_ap_led/__init__.py
+++ custom_components/unifi_ap_led/__init__.py
@@ -1,18 +1,36 @@
-
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+from .client import UnifiAPClient
+from .const import DOMAIN, CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_SITE, CONF_VERIFY_SSL
 
-DOMAIN = "unifi_ap_led"
+PLATFORMS = ["switch", "button"]
 
-async def async_setup_entry(hass: HomeAssistant, entry):
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "switch")
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up UniFi AP LED from a config entry."""
+    data = entry.data
+    client = UnifiAPClient(
+        host=data[CONF_HOST],
+        username=data[CONF_USERNAME],
+        password=data[CONF_PASSWORD],
+        site=data.get(CONF_SITE, "default"),
+        verify_ssl=data.get(CONF_VERIFY_SSL, True)
     )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "button")
-    )
+    
+    # Verify connection
+    if not await client.login():
+        return False
+    
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = client
+    
+    # Set up platforms
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry):
-    await hass.config_entries.async_forward_entry_unload(entry, "switch")
-    await hass.config_entries.async_forward_entry_unload(entry, "button")
-    return True
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        client = hass.data[DOMAIN].pop(entry.entry_id)
+        await client.close()
+    return unload_ok
