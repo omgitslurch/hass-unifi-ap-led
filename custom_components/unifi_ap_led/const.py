f7abdb0ffc7b6768753ce2288ca15e8093afcc31
Adam Cowell <lurch@lurch.uk>
2025-06-22 15:29:46 +0100

    Update const.py

--- custom_components/unifi_ap_led/const.py
+++ custom_components/unifi_ap_led/const.py
@@ -1,4 +1,12 @@
-
 DOMAIN = "unifi_ap_led"
 CONF_SITE = "site"
-CONF_MAC = "mac"
+CONF_AP_MAC = "ap_mac"
+CONF_VERIFY_SSL = "verify_ssl"
+
+# Error messages for translations
+ERRORS = {
+    "cannot_connect": "Unable to connect to UniFi Controller",
+    "invalid_auth": "Invalid authentication",
+    "no_aps": "No access points found",
+    "no_new_aps": "No additional access points available"
+}
